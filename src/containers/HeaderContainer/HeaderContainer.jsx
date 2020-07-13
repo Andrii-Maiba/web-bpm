a948e77a5f2a28b7e6383007d530511b3d510d72
Kate Fedorova <k_phedorova@ukr.net>
2020-07-13 14:23:27 +0300

    chore: make Header adaptive

--- src/containers/HeaderContainer/HeaderContainer.jsx
+++ src/containers/HeaderContainer/HeaderContainer.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {Menu, Header, Image, Button} from 'semantic-ui-react';
+import {Menu, Header, Image, Button, Icon} from 'semantic-ui-react';
 import {NavLink} from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 import {connect} from 'react-redux';
 import compose from '../../utils/compose';
 import withServices from '../../components/hocs/withServices';
@@ -9,40 +9,85 @@ import logo from '../../logo.svg';
 import {getUsersLocaleData} from '../../actions/usersLocaleDataAction';
 
 class HeaderNavContainer extends Component {
+    state = {isStackedMenu: true}
+
+    handleMenuStackedIconClick = e => {
+        e.preventDefault();
+        const isStacked = !this.state.isStackedMenu;
+        this.setState({isStackedMenu: isStacked});
+    }
+
     render() {
         return (
             <Menu stackable fixed='top' fluid>
-                <Menu.Item href="/">
-                    <Header as='h3' color='blue'>
-                        <Image src={logo}/>Web BPM</Header>
+                <Menu.Item href="/" className="item menu__item">
+                    <Header as='h3' className="menu__header" color='blue'><Image src={logo}/>Web BPM</Header>
+                    <div className='menu__content-stacked'>
+                        <Button.Group className='menu__buttons-lang-stacked'>
+                            {this.props.locale === "uk" ?
+                                <Button attached='left' active compact basic color="blue"
+                                        onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button> :
+                                <Button attached='left' compact basic color="blue"
+                                        onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button>}
+                            {this.props.locale === "en" ?
+                                <Button attached='right' active compact basic color="blue"
+                                        onClick={() => this.props.handleLangBtnClick("en")}>EN</Button> :
+                                <Button attached='right' compact basic color="blue"
+                                        onClick={() => this.props.handleLangBtnClick("en")}>EN</Button>}
+                        </Button.Group>
+                        {this.state.isStackedMenu ? <Icon color='blue' size='large' name='bars'
+                                                          onClick={e => this.handleMenuStackedIconClick(e)}/> :
+                            <Icon color='blue' size='large' name='close'
+                                  onClick={e => this.handleMenuStackedIconClick(e)}/>}
+                    </div>
                 </Menu.Item>
-                <Menu.Item exact as={NavLink} to="/"
-                           className="item"
+                {this.state.isStackedMenu ? <Menu.Item exact as={NavLink} to="/"
+                           className="item menu__item-stackable"
                            activeClassName="active">
                     <Header as='h4'>
                         <FormattedMessage id="header.maker-tasklist-link" defaultMessage="Задачі Автора"/>
                     </Header>
-                </Menu.Item>
-                <Menu.Item as={NavLink} to="/checker"
-                           className="item"
+                </Menu.Item> : <Menu.Item exact as={NavLink} to="/"
+                                          className="item"
+                                          activeClassName="active">
+                    <Header as='h4'>
+                        <FormattedMessage id="header.maker-tasklist-link" defaultMessage="Задачі Автора"/>
+                    </Header>
+                </Menu.Item>}
+                {this.state.isStackedMenu ? <Menu.Item as={NavLink} to="/checker"
+                           className="item menu__item-stackable"
                            activeClassName="active">
                     <Header as='h4'>
                         <FormattedMessage id="header.checker-tasklist-link" defaultMessage="Задачі Контролера"/>
                     </Header>
-                </Menu.Item>
-                <Menu.Item as={NavLink} to="/archive"
-                           className="item"
-                           activeClassName="active">
+                </Menu.Item> : <Menu.Item as={NavLink} to="/checker"
+                                          className="item"
+                                          activeClassName="active">
+                    <Header as='h4'>
+                        <FormattedMessage id="header.checker-tasklist-link" defaultMessage="Задачі Контролера"/>
+                    </Header>
+                </Menu.Item>}
+                {this.state.isStackedMenu ? <Menu.Item as={NavLink} to="/archive"
+                                                       className="item menu__item-stackable"
+                                                       activeClassName="active">
                     <Header as='h4'><FormattedMessage id="header.archive-link" defaultMessage="Архів"/></Header>
-                </Menu.Item>
-                <Menu.Item position='right' borderless>
+                </Menu.Item> : <Menu.Item as={NavLink} to="/archive"
+                                          className="item"
+                                          activeClassName="active">
+                    <Header as='h4'><FormattedMessage id="header.archive-link" defaultMessage="Архів"/></Header>
+                </Menu.Item>}
+                <Menu.Item position='right' className="item menu__item-lang">
                     <Button.Group floated='right'>
                         {this.props.locale === "uk" ?
-                            <Button attached='left' active compact basic color="blue" onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button> :
-                            <Button attached='left' compact basic color="blue" onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button>}
+                            <Button attached='left' active compact basic color="blue"
+                                    onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button> :
+                            <Button attached='left' compact basic color="blue"
+                                    onClick={() => this.props.handleLangBtnClick("uk")}>UA</Button>}
                         {this.props.locale === "en" ?
-                            <Button attached='right' active compact basic color="blue" onClick={() => this.props.handleLangBtnClick("en")}>EN</Button> :
-                            <Button attached='right' compact basic color="blue" onClick={() => this.props.handleLangBtnClick("en")}>EN</Button>}
+                            <Button attached='right' active compact basic color="blue"
+                                    onClick={() => this.props.handleLangBtnClick("en")}>EN</Button> :
+                            <Button attached='right' compact basic color="blue"
+                                    onClick={() => this.props.handleLangBtnClick("en")}>EN</Button>}
                     </Button.Group>
                 </Menu.Item>
             </Menu>
